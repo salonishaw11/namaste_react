50445b9524a738baa8189efd7d6b0e805db83f0f
salonishaw11 <121348227+salonishaw11@users.noreply.github.com>
2023-03-25 22:10:31 +0530

    chapter2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div",
     {id: "parent"},
     React.createElement("h1",
         { id: "chiild" },
-    "I am child"
+    "I am  a child"
     )    
 )
 
 
-        const root= ReactDOM.createRoot(document.getElementById("root"))
-        root.render(heading)
+const root= ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
